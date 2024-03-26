ab9119e3ecb1986125c25c6a22ff6433b0393ef2
Sowmiya muthukrishnan <sowmiya.2.m@eviden.com>
2024-03-26 21:42:38 +0530

    commit

--- src/main/java/com/product/datateam/DatateamApplication.java
+++ src/main/java/com/product/datateam/DatateamApplication.java
@@ -2,8 +2,13 @@ package com.product.datateam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.product.datateam.controller.DataController;
 
 @SpringBootApplication
+//@ComponentScan(basePackageClasses = DataController.class)
+@ComponentScan(basePackages="com.product.datateam")
 public class DatateamApplication {
 
 	public static void main(String[] args) {
