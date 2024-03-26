ca90cc178723af00d88c44a637c172723e603d9c
Sowmiya muthukrishnan <sowmiya.2.m@eviden.com>
2024-03-26 10:35:30 +0530

    commit

---
+++ src/test/java/com/product/datateam/DatateamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.datateam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatateamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
